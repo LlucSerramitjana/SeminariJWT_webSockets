92d133e07fb59da53aa65816f7936d74c7682d2a
LlucSerramitjana <lluc.serramitjana@estudiantat.upc.edu>
2022-03-25 12:14:50 +0100

    secondcommit

---
+++ src/models/User.ts
@@ -0,0 +1,13 @@
+import {Schema, model} from 'mongoose';
+
+const UserSchema = new Schema({
+    name: {type: String, required:true},
+    age: {type: String, required:true},
+    password: {type: String, required:true},
+    creationDate: {type: Date, default:Date.now},
+    roles: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Role'
+    }]
+});
+export default model('User', UserSchema);
